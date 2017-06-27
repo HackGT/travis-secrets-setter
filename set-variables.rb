721e777e7877956fe93804a4093f35fdb0247706
Michael Eden <themichaeleden@gmail.com>
2017-06-27 13:58:54 -0700

    Enable all repos & push to biodomes.

--- set-variables.rb
+++ set-variables.rb
@@ -11,9 +11,47 @@ keys = [
 	'DOCKER_PASSWORD',
 ]
 
+puts %x(
+	set -euox
+	mkdir -p biodomes/dev
+	cd biodomes
+	git init
+	git config user.name "Michael Eden"
+	git config user.email "themichaeleden@gmail.com"
+
+	git remote add upstream "https://#{ENV['GH_TOKEN']}@github.com/HackGT/biodomes.git"
+	git fetch upstream
+	git reset upstream/master
+)
+
+raise 'Could not set up git repo!' if $?.exitstatus != 0
+
 repos.each do |repo|
+	# Set all the secrets
 	keys.each do |key|
 		puts "Setting env var '#{key}' on project '#{repo.slug}'"
 		repo.env_vars.upsert(key, "'#{ENV[key]}'", public: false)
 	end
+
+	puts "Enabling project #{repo.slug}."
+	repo.enable
+
+	biodome_path = "biodomes/dev/#{File.basename repo.slug}"
+	unless File.exist? biodome_path
+		puts 'Creating default config in biodomes.'
+		File.open biodome_path, 'w' do |file|
+			file.write("git: https://github.com/#{repo.slug}.git")
+		end
+	end
 end
+
+puts %x(
+	set -euox
+	cd biodomes
+	touch .
+	git add -A .
+	git commit -m 'Automated Commit: add new repos.'
+	git push -q upstream HEAD:master
+)
+
+raise 'Could not push to git!' if $?.exitstatus != 0
