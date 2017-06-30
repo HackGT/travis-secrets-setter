6953ba34ebb263493adfcd96c131d789f9a02712
Michael Eden <themichaeleden@gmail.com>
2017-06-30 15:22:46 -0700

    Try enabling before writing secrets?

--- set-variables.rb
+++ set-variables.rb
@@ -3,62 +3,63 @@ require 'travis'
 Travis.github_auth(ENV['GH_TOKEN'])
 
 repos = Travis::Repository
-	.find_all(owner_name: 'HackGT')
-	.reject{|repo| repo.slug == 'HackGT/travis-secrets-setter'}
-	.select{|repo| Travis.user.admin_access.include?(repo)}
+        .find_all(owner_name: 'HackGT')
+        .reject { |repo| repo.slug == 'HackGT/travis-secrets-setter' }
+        .select { |repo| Travis.user.admin_access.include?(repo) }
 
-keys = [
-    'DOCKER_PASSWORD',
-    'GH_TOKEN',
-    'CLOUDFLARE_ZONE',
-    'CLOUDFLARE_EMAIL',
-    'CLOUDFLARE_AUTH',
-]
-
-
-puts %x(
-	set -euox
-	mkdir -p biodomes/dev
-	cd biodomes
-	git init
-	git config user.name "Michael Eden"
-	git config user.email "themichaeleden@gmail.com"
-
-	git remote add upstream "https://#{ENV['GH_TOKEN']}@github.com/HackGT/biodomes.git"
-	git fetch upstream
-	git reset --hard upstream/master
+keys = %w(
+  DOCKER_PASSWORD
+  GH_TOKEN
+  CLOUDFLARE_ZONE
+  CLOUDFLARE_EMAIL
+  CLOUDFLARE_AUTH
 )
+
+puts `
+  set -euox
+  mkdir -p biodomes/dev
+  cd biodomes
+  git init
+  git config user.name "Michael Eden"
+  git config user.email "themichaeleden@gmail.com"
+
+  git remote add upstream \
+    "https://#{ENV['GH_TOKEN']}@github.com/HackGT/biodomes.git"
+  git fetch upstream
+  git reset --hard upstream/master
+`
 
 raise 'Could not set up git repo!' if $?.exitstatus != 0
 
 repos.each do |repo|
-	# Set all the secrets
-	keys.each do |key|
-		puts "Setting env var '#{key}' on project '#{repo.slug}'"
-		repo.env_vars.upsert(key, "'#{ENV[key]}'", public: false)
-	end
+  puts "Enabling project #{repo.slug}."
+  repo.enable
 
-	puts "Enabling project #{repo.slug}."
-	repo.enable
+  # Set all the secrets
+  keys.each do |key|
+    puts "Setting env var '#{key}' on project '#{repo.slug}'"
+    repo.env_vars.upsert(key, "'#{ENV[key]}'", public: false)
+  end
 
-	biodome_path = "biodomes/dev/#{File.basename repo.slug.downcase}.yaml"
-	unless File.exist? biodome_path
-		puts 'Creating default config in biodomes.'
-		File.open biodome_path, 'w' do |file|
-			file.write("git: https://github.com/#{repo.slug}.git")
-		end
-	end
+  biodome_path = "biodomes/dev/#{File.basename repo.slug.downcase}.yaml"
+
+  next unless File.exist? biodome_path
+
+  puts 'Creating default config in biodomes.'
+  File.open biodome_path, 'w' do |file|
+    file.write("git: https://github.com/#{repo.slug}.git")
+  end
 end
 
-puts %x(
-	set -euox
-	cd biodomes
-	touch .
-	if [ -n "$(git status -s)" ]; then
-		git add -A .
-		git commit -m 'Automated Commit: add new repos.'
-		git push -q upstream HEAD:master
-	fi
-)
+puts `
+  set -euox
+  cd biodomes
+  touch .
+  if [ -n "$(git status -s)" ]; then
+    git add -A .
+    git commit -m 'Automated Commit: add new repos.'
+    git push -q upstream HEAD:master
+  fi
+`
 
 raise 'Could not push to git!' if $?.exitstatus != 0
